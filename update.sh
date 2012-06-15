ce3dd381b8524ad7c86dcaa2651f160a971b792a
bugcloud <bugcloudisco@bugcloud.com>
2012-06-15 10:31:21 +0900

    update.shが相対パスになってないの気づかなかった

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 FEEDS=( daily weekly monthly)
 for file in ${FEEDS[@]}
 do
-  wget -O $HOME/Dropbox/Public/vocalo/feed/$file.xml http://www.nicovideo.jp/ranking/fav/$file/vocaloid?rss=atom
+  wget -O ${PWD}/feed/$file.xml http://www.nicovideo.jp/ranking/fav/$file/vocaloid?rss=atom
 done
 
